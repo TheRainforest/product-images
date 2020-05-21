83897b2034fbabb379b2dc706ce291917eb1fbb8
Eric Song <eric.song@columbia.edu>
2020-05-20 23:42:10 -0700

    Relocate file to database directory

---
+++ server/database/dummyData.js
@@ -0,0 +1,27 @@
+/* eslint-disable linebreak-style */
+const data = [];
+let arr = [];
+
+const run = function run() {
+  for (let i = 1; i <= 101; i += 1) {
+    if (arr.length !== 5) {
+      arr.push(`https://single-page-items.s3.us-east-2.amazonaws.com/img${i}.jpg`);
+    } else {
+      const obj = {
+        altImages: arr,
+      };
+      data.push(obj);
+      arr = [];
+      arr.push(`https://single-page-items.s3.us-east-2.amazonaws.com/img${i}.jpg`);
+    }
+  }
+  arr = [];
+};
+
+run();
+run();
+run();
+run();
+run();
+
+module.exports = data;
