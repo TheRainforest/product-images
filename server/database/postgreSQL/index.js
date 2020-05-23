855ea674a6733feb14582cb158c702c8bec2747f
Eric Song <eric.song@columbia.edu>
2020-05-22 20:38:13 -0700

    Create connection to postgreSQL database

---
+++ server/database/postgreSQL/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+const path = require('path');
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+});
+
+const csvFile = path.join(__dirname, '../product-images.csv');
+
+pool.connect()
+  .then(() => pool.query(`COPY items FROM '${csvFile}' WITH DELIMITER '|' CSV HEADER`))
+  .then(() => console.log('csv file successfully imported'))
+  .catch((err) => console.error(err));
