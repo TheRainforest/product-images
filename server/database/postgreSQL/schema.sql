977a0aa8bc775b7bcf18e92442f1107b7d192c5b
Eric Song <eric.song@columbia.edu>
2020-05-27 01:03:25 -0700

    Refactor postgreSQL setup

--- server/database/postgreSQL/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS productImages;
-DROP TABLE IF EXISTS items;
-CREATE DATABASE productImages;
-\c productImages;
-
-CREATE TABLE items (
-  id SERIAL NOT NULL PRIMARY KEY,
-  images text[]
-);
