df43abaaa264ddb774f93846a0ad8fa3e91a4c27
Eric Song <eric.song@columbia.edu>
2020-06-02 21:53:07 -0700

    Rephrase HTTP request error message

--- server/routes/mongoRoutes.js
+++ server/routes/mongoRoutes.js
@@ -9,25 +9,25 @@ router.use(express.urlencoded());
 router.post('/', (req, res) => {
   db.createItem(req.body.images)
     .then((rows) => res.status(200).json(rows))
-    .catch((err) => res.status(500).json(err));
+    .catch((err) => res.status(400).send("Error in POST request"));
 });
 
 router.get('/:id', (req, res) => {
   db.getItem(req.params.id)
     .then((rows) => res.status(200).json(rows))
-    .catch((err) => res.status(500).json(err));
+    .catch((err) => res.status(400).send("Error in GET request"));
 });
 
 router.put('/:id', (req, res) => {
   db.updateItem(req.params.id, req.params.imageIndex, req.params.image)
     .then((rows) => res.status(200).json(rows))
-    .catch((err) => res.status(500).json(err));
+    .catch((err) => res.status(400).send("Error in PUT request"));
 });
 
 router.delete('/:id', (req, res) => {
   db.deleteItem(req.params.id)
     .then((rows) => res.status(200).json(rows))
-    .catch((err) => res.status(500).json(err));
+    .catch((err) => res.status(400).send("Error in DELETE request"));
 });
 
 module.exports = router;
