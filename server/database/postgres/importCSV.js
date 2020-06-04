d5d8aeb1e1a0b0ff74617ec366bdae75ffc46e77
Eric Song <eric.song@columbia.edu>
2020-06-04 16:51:13 -0700

    Add password field for connection pool

--- server/database/postgres/importCSV.js
+++ server/database/postgres/importCSV.js
@@ -5,6 +5,7 @@ const path = require('path');
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
+  password: process.env.PG_PASSWORD,
   database: process.env.PG_DB,
   port: process.env.PG_PORT,
 });
