c29afcd72ea6221f7d5ae005c1227be14c8a512d
Eric Song <eric.song@columbia.edu>
2020-06-04 17:08:51 -0700

    Add password field for connection pool

--- server/database/postgres/index.js
+++ server/database/postgres/index.js
@@ -4,12 +4,12 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
+  password: process.env.PG_PASSWORD,
   database: process.env.PG_DB,
   port: process.env.PG_PORT,
 });
 
 pool.connect();
-
 
 const getItem = (id) => {
   return pool.query(`SELECT * FROM items WHERE itemId = ${id}`);
